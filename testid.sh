a4668e5412309dc10235f4f25bc4aec176c66fd8
Alex Mamach <garthand@github.com>
2023-02-11 19:04:03 +0000

    Basic counting test

--- testid.sh
+++ testid.sh
@@ -6,7 +6,10 @@ IFS=$'\n\t'
 
 function genid {
 	(flock 222
-	echo "Test test"
+	last_count=$(cat countfile)
+	count=$((last_count+1))
+	echo "$count" > countfile
+	echo "$count"
 	) 222>.genid_lockfile
 }
 
