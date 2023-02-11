212f9cc6c0a33b2abe1144d9446493128c94dec4
Alex Mamach <garthand@github.com>
2023-02-11 19:13:31 +0000

    Added comments describing work so far

--- testid.sh
+++ testid.sh
@@ -5,11 +5,19 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function genid {
+	# Define variables locally
 	local last_number
 	local new_number
+	# Create a file lock on descriptor 222
+	# If another process already has a lock on 222,
+	# wait for the file lock to be released before continuing
 	(flock 222
+	# Get the last used ID from the last_id file
 	last_number=$(cat .last_id)
+	# Increment the last_id by one
 	new_number=$((last_number + 1))
+	# Print the result to stdout and save it
+	# to the last_id file
 	echo "$new_number" > .last_id
 	echo "$new_number"
 	) 222>.genid_lockfile
