2dd59b379b1ab2f0e02136a8de5cd9b06fb3fddf
Alex Mamach <garthand@github.com>
2023-02-11 19:53:49 +0000

    Renamed last_id to genid_last_id

--- testid.sh
+++ testid.sh
@@ -12,20 +12,20 @@ function genid {
 	# If another process already has a lock on 222,
 	# wait for the file lock to be released before continuing
 	(flock 222
-	# Create the last_id file if it does not yet exist
+	# Create the genid_last_id file if it does not yet exist
 	# and start the ID generation from 00000
-	if ! test -f .last_id
+	if ! test -f .genid_last_id
 	then
-		echo 00000 > .last_id
+		echo 00000 > .genid_last_id
 	fi
-	# Get the last used ID from the last_id file
-	last_number=$(cat .last_id)
-	# Increment the last_id by one and format it
+	# Get the last used ID from the genid_last_id file
+	last_number=$(cat .genid_last_id)
+	# Increment the genid_last_id by one and format it
 	# with leading zeros if necessary
 	new_number=$(printf "%05d" "$(echo "$last_number + 1"|bc -l)")
 	# Print the result to stdout and save it
-	# to the last_id file
-	echo "$new_number" | tee .last_id
+	# to the genid_last_id file
+	echo "$new_number" | tee .genid_last_id
 	# Write the (empty) contents of file descriptor 222
 	# to the file to release the lock
 	) 222>.genid_lockfile
